41f0f0eb3177dfb4f357db1aff97561af0f92346
asiasyouasias <kda0327@gmail.com>
2026-07-10 16:48:40 +0900

    [init]백엔드 Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/aivle13/fin_audit_ai/FinAuditAiApplication.java
@@ -0,0 +1,13 @@
+package com.aivle13.fin_audit_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinAuditAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinAuditAiApplication.class, args);
+	}
+
+}
