e45f99137fb7b573313e952b75df7c9ea8ad4439
JoJimi <duddnddnfl@naver.com>
2026-07-16 11:16:12 +0900

    [refactor/#5] BaseEntity 적용을 위한 약간의 수정

--- src/main/java/com/aivle13/fin_audit_ai/health/HealthCheck.java
+++ src/main/java/com/aivle13/fin_audit_ai/health/HealthCheck.java
@@ -1,27 +1,21 @@
 package com.aivle13.fin_audit_ai.health;
 
+import com.aivle13.fin_audit_ai.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class HealthCheck {
+public class HealthCheck extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private LocalDateTime checkedAt;
-
-    public static HealthCheck now() {
-        HealthCheck h = new HealthCheck();
-        h.checkedAt = LocalDateTime.now();
-        return h;
+    public static HealthCheck create() {
+        return new HealthCheck();
     }
 }
