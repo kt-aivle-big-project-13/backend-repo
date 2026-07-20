cf966569295b2e91dd700dfefe9a91e58774d288
asiasyouasias <kda0327@gmail.com>
2026-07-20 16:36:04 +0900

    [feat/#14] Audit 생성 서비스 구현

---
+++ src/main/java/com/aivle13/fin_audit_ai/domain/audit/repository/AuditRepository.java
@@ -0,0 +1,4 @@
+package com.aivle13.fin_audit_ai.domain.audit.repository;
+
+public interface AuditRepository extends JpaRepository<Audit, Long> {
+}
---
+++ src/main/java/com/aivle13/fin_audit_ai/domain/audit/service/AuditService.java
@@ -0,0 +1,24 @@
+package com.aivle13.fin_audit_ai.domain.audit.service;
+
+import com.aivle13.fin_audit_ai.domain.audit.repository.AuditRepository;
+import com.aivle13.fin_audit_ai.domain.audit.type.AuditStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuditService {
+
+    private final AuditRepository auditRepository;
+
+    public Audit create(Long userId, AiModel aiModel, String datasetPath) {
+        Audit audit = Audit.builder()
+                .userId(userId)
+                .aiModel(aiModel)
+                .datasetPath(datasetPath)
+                .status(AuditStatus.UPLOADED)
+                .build();
+
+        return auditRepository.save(audit);
+    }
+}
