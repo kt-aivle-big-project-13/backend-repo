7e0a0f47d8828469b2097d314c3f2a9ad283e0ed
JoJimi <duddnddnfl@naver.com>
2026-07-22 15:28:27 +0900

    [chore/#49] GlobalExceptionHandler 잔여 디버깅 주석 정리

--- src/main/java/com/aivle13/fin_audit_ai/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aivle13/fin_audit_ai/global/exception/GlobalExceptionHandler.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.access.AccessDeniedException;   // ← 수정
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -17,7 +17,7 @@ import org.springframework.web.context.request.async.AsyncRequestNotUsableExcept
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.resource.NoResourceFoundException;  // ← Boot 3.2+
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.List;
 
