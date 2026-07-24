d9332cf12165f0e523b1c1bf7b67e4e0b6f243ae
momiju <ohj501891@gmail.com>
2026-07-24 15:58:21 +0900

    feat: Google reCAPTCHA 검증 API 연동

--- src/main/java/com/aivle13/fin_audit_ai/domain/auth/dto/response/RecaptchaVerifyResponse.java
+++ src/main/java/com/aivle13/fin_audit_ai/domain/auth/dto/response/RecaptchaVerifyResponse.java
@@ -1,9 +1,11 @@
 package com.aivle13.fin_audit_ai.domain.auth.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record RecaptchaVerifyResponse(
 
         boolean success,
