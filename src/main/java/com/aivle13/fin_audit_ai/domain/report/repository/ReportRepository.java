ba12b29985764f6a9b5fe36a668a48c3aefeca2a
Dajin-01 <newdajin@gmail.com>
2026-07-30 17:11:01 +0900

    [feat/#152] 설명가능성 PDF·Word 리포트 연동

--- src/main/java/com/aivle13/fin_audit_ai/domain/report/repository/ReportRepository.java
+++ src/main/java/com/aivle13/fin_audit_ai/domain/report/repository/ReportRepository.java
@@ -1,6 +1,7 @@
 package com.aivle13.fin_audit_ai.domain.report.repository;
 
 import com.aivle13.fin_audit_ai.domain.report.entity.ReportEntity;
+import com.aivle13.fin_audit_ai.domain.report.type.ReportFormat;
 import com.aivle13.fin_audit_ai.domain.report.type.ReportType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,10 +11,11 @@ public interface ReportRepository
         extends JpaRepository<ReportEntity, Long> {
 
     Optional<ReportEntity>
-    findFirstByAudit_IdAndAudit_User_IdAndReportTypeOrderByCreatedAtDescIdDesc(
+    findFirstByAudit_IdAndAudit_User_IdAndReportTypeAndFormatOrderByCreatedAtDescIdDesc(
             Long auditId,
             Long userId,
-            ReportType reportType
+            ReportType reportType,
+            ReportFormat format
     );
 
     Optional<ReportEntity>
